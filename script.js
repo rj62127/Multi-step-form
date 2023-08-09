830eda4726d1ca994dc3b5a88546ce1e67e9dd91
Rajan Kumar Jha <rjha2.718@gmail.com>
2023-08-09 14:34:21 +0530

    Step 1 - 4

--- script.js
+++ script.js
@@ -1,98 +1,141 @@
 // declare variables and store elements
+
 const sidebarStep = document.querySelectorAll('.indecater__num');
-const formSteps = document.querySelectorAll('.step');
-const stepsContainer = document.getElementById('steps__container');
+const formStep = document.querySelectorAll('.step');
+const form = document.getElementById('form');
 const planCards = document.querySelectorAll('.plan__card');
 const addsonCards = document.querySelectorAll('.addon__card');
 const changePlanBtn = document.getElementById('change-plan');
-const nextButton = document.getElementById('next-button');
 
 // empty object to store selected plan, price and duration
 let selectedPlan = {};
-
 // function for storing selected add-on plan
 const selectedAddsOn = () => {
+  // create empty array to store plan details
+  let addOnArr = [];
+  // loop add-on card to find selected add-on cards
+  addsonCards.forEach((card) => {
+    // select plan name, price and plan duration
+    let price = card.querySelector('.sbscription__price').textContent;
+    let name = card.querySelector('.card__name').textContent;
+    let planDur = card.querySelector('.sbscription__duration').textContent;
+    // push selected cards details to addOnArr array
+    if (card.classList.contains('selected')) {
+      addOnArr.push({
+        price,
+        name,
+        planDur,
+      });
+    }
+  });
+  // return addOnArr array
+  return addOnArr;
+};
 
-    // create emplty array to store plan details
-    let addOnArr = [];
+// plan prices
+const monthlyPlanPrices = [9, 12, 15];
+const yearlyPlanPrices = [90, 120, 150];
+const monthlyAdsOnPrice = [1, 2, 2];
+const yearlyAdsOnPrice = [10, 20, 20];
 
-    // loop add-on card to find selected add-on cards
-    addsonCards.forEach((card) => {
+// function to change price and duration of the given card
+const setplan = (card, price, duration) => {
+  card.forEach((card, i) => {
+    card.querySelector('.sbscription__price').textContent = `${price[i]}`;
+    card.querySelector('.sbscription__duration').textContent = `${duration}`;
+  });
+};
 
-      // select plan name, price and plan duration
-
-      let price = card.querySelector('.sbscription__price').textContent;
-      let name = card.querySelector('.card__name').textContent;
-      let planDur = card.querySelector('.sbscription__duration').textContent;
-      // push selected cards details to addOnArr array
-      if (card.classList.contains('selected')) {
-        addOnArr.push({
-          price,
-          name,
-          planDur,
-        });
-      }
-    });
-    // return addOnArr array
-    return addOnArr;
-  };
-
-
-  // plan prices
-  const monthlyPlanPrices = [9, 12, 15];
-  const yearlyPlanPrices = [90, 120, 150];
-  const monthlyAdsOnPrice = [1, 2, 2];
-  const yearlyAdsOnPrice = [10, 20, 20];
-
-  // function to change price and duration of the given card
-  const setplan = (card, price, duration) => {
-    card.forEach((card, i) => {
-      card.querySelector('.sbscription__price').textContent = `${price[i]}`;
-      card.querySelector('.sbscription__duration').textContent = `${duration}`;
-    });
-  };
-
-  // set default price and duration of the cards
+// set default price and duration of the cards
 setplan(planCards, monthlyPlanPrices, 'mo');
 setplan(addsonCards, monthlyAdsOnPrice, 'mo');
 
+
 // BUTTONS
 
+const nextBtn = document.getElementById('next-button');
+const prevBtn = document.getElementById('prev-button');
 
-function nextButtonClick(){
-  var activeStepIndex;
-  var activeStep;
-  for(i=0;i<formSteps.length;i++){
-    id = formSteps[i].getAttribute("id");
-    className = formSteps[i].getAttribute("class");
-    if(className.search("active") != -1){
-      activeStep = formSteps[i];
-      activeStepIndex = i;
-      break;
+// step number
+let stepNum = 0;
+
+// function for show warning text when plan not selected and next button pressed
+const selectPlanError = (text) => {
+  document.getElementById('select-plan-error').textContent = text;
+};
+
+//  next step button
+nextBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (stepNum == 0) {
+    // form validation
+    if (!formValidation()) return;
+    stepNum++;
+    showStep(stepNum);
+  } else if (stepNum === 1) {
+    // show warning if plan not selected
+    if (Object.entries(selectedPlan).length ==  0) {
+      return selectPlanError('Please select a plan');
     }
-  }
-  if(activeStepIndex != formSteps.length-1){
-    activeStep.classList.remove("active");
-    formSteps[activeStepIndex+1].classList.add("active");
-  }
-}
+    stepNum++;
+    showStep(stepNum);
+  } else if (stepNum === 2) {
+    // render list of selected plan and add-on with price
+    renderTotal();
+    stepNum++;
+    showStep(stepNum);
+  } else if (stepNum === 3) {
+    stepNum++;
+    showStep(stepNum);
+  } else return;
+});
 
-function prevButtonClick() {
-  
-  var activeStepIndex;
-  var activeStep;
-  for(i=0;i<formSteps.length;i++){
-    id = formSteps[i].getAttribute("id");
-    className = formSteps[i].getAttribute("class");
-    if(className.search("active") != -1){
-      activeStep = formSteps[i];
-      activeStepIndex = i;
-      break;
+// handle previus step button
+prevBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  stepNum--;
+  return showStep(stepNum);
+});
+
+// fuction for handle steps
+const showStep = (x) => {
+  // remove select plan warning
+  selectPlanError('');
+
+  // handle sidebar step
+  if (x < sidebarStep.length) {
+    // remove active class from all sidebar step
+    for (let i = 0; i < sidebarStep.length; i++) {
+      sidebarStep[i].classList.remove('active');
     }
-  }
-  if(activeStepIndex != 0 ){
-    activeStep.classList.remove("active");
-    formSteps[activeStepIndex-1].classList.add("active");
+    // adds the "active" class to the current sidebar step
+    sidebarStep[x].classList.add('active');
   }
 
-}
+  // handle form step
+  if (x < formStep.length) {
+    if (x == 0) {
+      // hide previuos button on the first step
+      prevBtn.classList.add('hidden');
+      prevBtn.setAttribute('disabled', '');
+    } else if (x === 4) {
+      // hide buttons
+      nextBtn.parentElement.classList.add('hidden');
+    } else {
+      // show buttons
+      prevBtn.classList.remove('hidden');
+      prevBtn.removeAttribute('disabled');
+    }
+    // change next step button inner text to 'confirm' on the step3
+    x === 3
+      ? (nextBtn.textContent = 'Confirm')
+      : (nextBtn.textContent = 'Next step');
+
+    // removes the "active" class of all steps...
+    for (let i = 0; i < formStep.length; i++) {
+      formStep[i].classList.remove('active');
+    }
+    // adds the "active" class to the current step
+    formStep[x].classList.add('active');
+  }
+};
