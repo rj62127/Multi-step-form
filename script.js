f7753738d5485536fbdfb696bc96447b895a6e5c
Rajan Kumar Jha <rjha2.718@gmail.com>
2023-08-09 16:12:59 +0530

    PLAN & TOGGLE BUTTON,  ADD-ON

--- script.js
+++ script.js
@@ -139,3 +139,129 @@ const showStep = (x) => {
     formStep[x].classList.add('active');
   }
 };
+// show step on first load
+showStep(stepNum);
+
+// STEP-1 | PERSONAL INFO [ FORM-VALIDATION ]
+
+// function for adding warning
+const showError = (input, warningText) => {
+  input.classList.add('error');
+  input.parentElement.querySelector('.warning').textContent = warningText;
+};
+
+// function for remove warning
+const hideError = (input) => {
+  input.classList.remove('error');
+  input.parentElement.querySelector('.warning').textContent = '';
+};
+
+// select all form inputs
+const formInput = form.querySelectorAll('input');
+
+// function for form validation
+const formValidation = () => {
+  // check all inputs are valid using forEach loop
+  formInput.forEach((input) => {
+    // for username
+    if (input.name == 'userName') {
+      return input.value.length == 0
+        ? showError(input, 'Enter your name')
+        : hideError(input);
+    }
+    // for email
+    if (input.name == 'email') {
+      // verify email input value using regex
+      const emailRegExp = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+      return input.value.length == 0
+        ? showError(input, 'Enter email')
+        : !emailRegExp.test(input.value)
+        ? showError(input, 'Enter valid email')
+        : hideError(input);
+    }
+    // phone number
+    if (input.name == 'phone') {
+        return input.value.length == 0
+        ? showError(input, 'Enter your mobile number')
+        : hideError(input);
+    }
+  });
+  // return form validity
+  return form.checkValidity();
+};
+
+// STEP-2 | SELECT PLAN & TOGGLE BUTTON
+const toggle = document.getElementById('toggle');
+const yearlyBenefit = document.querySelectorAll('.yearly__benefit');
+const month = document.getElementById('monthly');
+const year = document.getElementById('yearly');
+
+// toggle button for changing plan duration yearly or monthly
+toggle.addEventListener('click', (e) => {
+  selectPlanError('');
+  // selelct toggle container
+  const toggle = e.target.parentElement;
+  // remove selected class from all plan card
+  planCards.forEach((card) => card.classList.remove('selected'));
+  // make selectedPlan object emply
+  selectedPlan = {};
+
+  // add active class to the toggle
+  toggle.classList.toggle('active');
+
+  // if toggle active [ yearly ] else [ monthaly ]
+  if (toggle.classList.contains('active')) {
+    yearlyBenefit.forEach((item) => item.classList.add('show'));
+    setplan(planCards, yearlyPlanPrices, 'yr');
+    setplan(addsonCards, yearlyAdsOnPrice, 'yr');
+    year.classList.add('selected__plan');
+    month.classList.remove('selected__plan');
+  } else {
+    setplan(planCards, monthlyPlanPrices, 'mo');
+    setplan(addsonCards, monthlyAdsOnPrice, 'mo');
+    yearlyBenefit.forEach((item) => item.classList.remove('show'));
+    month.classList.add('selected__plan');
+    year.classList.remove('selected__plan');
+  }
+});
+
+// Select plan card
+planCards.forEach((card) => {
+  card.addEventListener('click', (e) => {
+    selectPlanError('');
+    let target = e.currentTarget;
+    // remove selected class of all cards
+    planCards.forEach((card) => card.classList.remove('selected'));
+    // add selected class to the current card
+    target.classList.add('selected');
+
+    // store selected plan name, price and duration
+    let planName = target.querySelector('.card__name').textContent;
+    let planPrice = target.querySelector('.sbscription__price').textContent;
+    let planDur = target.querySelector('.sbscription__duration').textContent;
+
+    // add selected plan details to the selectedPlan object
+    return (selectedPlan = { planName, planPrice, planDur });
+  });
+});
+
+// STEP3 | ADD-ON
+addsonCards.forEach((card) => {
+  // addeventlistner to the add-on cards
+  card.addEventListener('click', (e) => {
+    let target = e.currentTarget;
+    let checkbox = target.querySelector('.checkbox');
+    // add selected class to the card
+    target.classList.toggle('selected');
+
+    // checked box checked if box is selected
+    if (target.classList.contains('selected')) {
+      return (checkbox.checked = true);
+    } else {
+      return (checkbox.checked = false);
+    }
+  });
+});
+
+// STEP-4 | FINISHING UP
+
